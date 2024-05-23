23b54d3123adfe61cc4899b50b9651d3ad41505c
daisyisibor <daisyisibor8@gmail.com>
2024-05-23 14:07:21 -0400

    added procedd.env to enable deploy on heroku

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const path = require('path');
 const apiRoute = require("./routes/apiRoutes");
 const htmlRoutes = require('./routes/htmlRoutes');
+require('dotenv').config(); // Load environment variables from .env file
 
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001; 
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('frontend'));
 
-
-app.use(apiRoute)
+app.use(apiRoute);
 app.use(htmlRoutes);
 
 app.listen(PORT, () => {
